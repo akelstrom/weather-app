4d20b1f3cf1ea01f66df0993f6cb4f4aa1426d0e
akelstrom <akelstrom@gmail.com>
2020-12-15 12:58:21 -0500

    local storage and done

--- assets/script.js
+++ assets/script.js
@@ -16,8 +16,9 @@ submitButtonEl.addEventListener("click", function (event) {
   console.log(cityName);
 
   displayTodaysWeather(cityName);
-  fiveDayDisplay(cityName);
+  saveCity(cityName);
 });
+
 
 //function to display todays weather
 function displayTodaysWeather(cityName) {
@@ -40,8 +41,8 @@ function displayTodaysWeather(cityName) {
 
       var lat = data.coord.lat;
       var lon = data.coord.lon;
-
-      fiveDayDisplay(lat,lon);
+ 
+      fiveDayDisplay(lat, lon);
       //need to fetch call a different API to get the UI index, this API uses lat and long rather than city name
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${apiKey}`
@@ -57,29 +58,69 @@ function displayTodaysWeather(cityName) {
 }
 
 //function to display 5 day weather
-function fiveDayDisplay(lat,lon) {
-  
-     fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${apiKey}`
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          console.log("5 day",data);
-          for (var i = 1; i < 6; i++) {
-            console.log(data.daily[i])
-            //use inner html to display 5 day forecast
-            document.querySelector("#week-weather").innerHTML +=   `
+function fiveDayDisplay(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.querySelector("#week-weather").innerHTML = "";
+      for (var i = 1; i < 6; i++) {
+        //use inner html to display 5 day forecast
+        document.querySelector("#week-weather").innerHTML += `
             <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-              <div class="card-header">${moment.unix(data.daily[i].dt).format("MM/DD/YYYY")} </div>
+              <div class="card-header">${moment
+                .unix(data.daily[i].dt)
+                .format("MM/DD/YYYY")} </div>
               <div class="card-body">
-              <h5 class="card-title"><img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" /> </h5>
+              <h5 class="card-title"><img src="http://openweathermap.org/img/wn/${
+                data.daily[i].weather[0].icon
+              }@2x.png" /> </h5>
               <p class= "card-text"> Temp: ${data.daily[i].temp.day}</p>
               <p class= "card-text"> Humidity: ${data.daily[i].humidity}</p>
                 </div>
-              `
-          }
-        });
-  
+              `;
+      }
+    });
 }
+
+function loadCities() {
+  document.querySelector("#city-list").innerHTML = "";
+  //parse into an array and save that value into a variable
+  var cityArray = JSON.parse(localStorage.getItem("cities"));
+
+  cityArray.forEach((city)=> {
+     document.querySelector("#city-list").innerHTML += `<li class="list-group-item city-item">${city}</li>`
+  }) 
+  //add city to the list
+  document.querySelectorAll(".city-item").forEach((city) => {
+    //add event listener, for each item in the array, when you click event function happens
+      city.addEventListener("click", function (event) {
+        event.preventDefault();
+        //get cityname from search input value
+        var cityName = this.textContent
+        //call the function that displays the weather data
+        displayTodaysWeather(cityName);
+      });
+  })
+}
+
+function saveCity(cityName) {
+  //if statement to check if there is local storge
+  //create local storage with empty array
+  if (!localStorage.getItem("cities")) {
+    localStorage.setItem("cities", JSON.stringify([]));
+  }
+  //take of value of the city input
+  var cityArray = JSON.parse(localStorage.getItem("cities"));
+  cityArray.push(cityName);
+
+  //save back to local storage
+  localStorage.setItem("cities", JSON.stringify(cityArray));
+  loadCities();
+}
+
+loadCities();
+
